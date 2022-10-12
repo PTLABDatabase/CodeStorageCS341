50bd0e28fb47fdc31b225f551e3937e035c580f5
PTLABDatabase <112571703+PTLABDatabase@users.noreply.github.com>
2022-10-12 14:05:05 -0700

    Update LL.java

--- CS341Assignment5/src/LL.java
+++ CS341Assignment5/src/LL.java
@@ -1,45 +1,54 @@
 public class LL {
-	static Node head = null;
-	static double sum = 0;
-	static double sd = 0;
-	static double sq = 0;
-	static double res = 0;
-	static double mean = 0;
-	static double ct = 0;
+	
+	//list of needed variables for computing sum, mean and standard deviation
+	
+	static Node head = null; // head of list 
+	static double sum = 0; // sum of all elements in list
+	static double sd = 0; // standard deviation of all elements in list
+	static double sq = 0;// square root variable
+	static double res = 0;// resultant of square root
+	static double mean = 0;// mean of elements in list
+	static double ct = 0; // count of all elements in list
 
+	// Enqueueing data into the list
+	
 	void push(int data)
 	{
-	    Node newNode = new Node(data);
-	    newNode.data = data;
-	    newNode.next = (head);
-	    (head) = newNode;
-	}
-
-	static void sumOfNodes(Node head) {
-		if (head == null)
-			return;
-		sumOfNodes(head.next);
-		sum = sum + head.data;
-	}
-
-	String sumOfNodesUtil(int count) {
-		ct  = count;
-		sumOfNodes(head);
-		mean = sum/count;
-		return "Sum of All Elements: " + sum + " | " + " Mean of All Elements: " + mean ;
+	    Node newNode = new Node(data); //creating a newNode
+	    newNode.data = data; // getting node data
+	    newNode.next = (head); // setting head to point to next node
+	    (head) = newNode; // head holds newNode
 	}
 	
-	static void sdOfNodes(Node head) {
-		if(head ==null)
+	//Calculate the sum of elements in list
+
+	static void sumOfNodes(Node head) {
+		if (head == null) //exit if head ==0
 			return;
-		sdOfNodes(head.next);
-		sd += Math.pow((head.data - mean), 2);
+		sumOfNodes(head.next); // calling sumOfNodes function with the next in list
+		sum = sum + head.data; // double variable sum added with new values
+	}
+
+	String sumOfNodesUtil(int count) { 
+		ct  = count; // get count from main class
+		sumOfNodes(head);// call on sum function
+		mean = sum/count;//calculate the mean -> sum/ count
+		return "Sum of All Elements: " + sum + " | " + " Mean of All Elements: " + mean ; // return sum and mean to main class
+	}
+	
+	//Calculate the standard deviation of elements in list
+	
+	static void sdOfNodes(Node head) {
+		if(head ==null) //exit if head ==0
+			return;
+		sdOfNodes(head.next); // calling sdOfNodes function with the next in list
+		sd += Math.pow((head.data - mean), 2); //double standard deviation variable added with computed variables
 	}
 	
 	String sdUtil() {
-		sdOfNodes(head);
-		sq = sd / ct;
-        res = Math.sqrt(sq);
-		return "Standard Deviation: " + res;
+		sdOfNodes(head);// call on sd function
+		sq = sd / ct;// calculating square root of the sd/count
+        	res = Math.sqrt(sq);//finally finding the square root of the sd variable
+		return "Standard Deviation: " + res; // return standard deviation to main class
 	}
 }
